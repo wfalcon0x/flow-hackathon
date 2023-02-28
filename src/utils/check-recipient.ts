1a808faa683c2e2fa1fa57808c3eaadac9d6ff28
wfalcon0x <>
2023-02-28 17:09:31 +0000

    Add function to check recipient is valid and can recieve a float

---
+++ src/utils/check-recipient.ts
@@ -0,0 +1,82 @@
+import * as fcl from "@onflow/fcl";
+
+fcl
+  .config()
+  .put("discovery.wallet", "https://fcl-discovery.onflow.org/testnet/authn")
+  .put("accessNode.api", "https://rest-testnet.onflow.org")
+  .put("0xFLOAT", "0x0afe396ebc8eee65")
+  .put("0xFIND", "0x35717efbbce11c74")
+
+export const validateRecipient = async (recipient: string, host: string, eventId: string) => {
+  const checkClaimedFromAddress = `
+  import FLOAT from 0xFLOAT
+  
+  pub fun main(hostAddress: Address, eventId: UInt64, accountAddress: Address): {String: AnyStruct} {
+    let acct = getAuthAccount(accountAddress)
+  
+    // Check if FLOAT storage path exists
+    if acct.borrow<&FLOAT.Collection>(from: FLOAT.FLOATCollectionStoragePath) == nil {
+      return { "message": "You'll need to already have any float in order to claim this float", "ok": false}
+    }
+    let floatEventCollection = getAccount(hostAddress).getCapability(FLOAT.FLOATEventsPublicPath)
+        .borrow<&FLOAT.FLOATEvents{FLOAT.FLOATEventsPublic}>()
+        ?? panic("Could not borrow the FLOAT Events Collection from the account.")
+    let floatEventPublic = floatEventCollection.borrowPublicEventRef(eventId: eventId)
+  
+    let claimed = floatEventPublic!.hasClaimed(account: accountAddress)
+  
+    // Check if FLOAT has been already claimed
+    if claimed != nil {
+      return { "message": "You'll only be able to claim this float once, thanks for trying again!", "ok": false, "claimed": claimed }
+    }
+    return { "message": "All systems go", "ok": true }
+  }
+  `
+
+  const findMatcher = /^[a-z0-9-]{3,16}.find$/
+  const walletAddressMatcher = /^0x[a-fA-F0-9]{16}$/g
+  const noSpaces = recipient.replace(/\s/g, "");
+  let addressToCheck = noSpaces
+  
+  if (findMatcher.test(noSpaces)) {
+    console.log('Checking .find address')
+    const code = `
+      import FIND from 0xFIND
+      pub fun main(name: String) : Address? {
+          return FIND.lookupAddress(name)
+      }
+    `;
+  
+    addressToCheck = await fcl.query({
+      cadence: code,
+      args: (arg: any, t: any) => [
+        arg(noSpaces, t.String),
+      ],
+    })
+    if (addressToCheck === null) {
+      return {
+        ok: false,
+        message: "No .find address found",
+      }
+    }
+  }
+  
+  if (walletAddressMatcher.test(addressToCheck)) {
+    console.log('Checking FLOATs')
+    const result = await fcl.query({
+      cadence: checkClaimedFromAddress,
+      args: (arg: any, t: any) => [
+        arg(host, t.Address),
+        arg(eventId, t.UInt64),
+        arg(addressToCheck, t.Address), 
+  
+      ],
+    });
+    return result
+  }
+
+  return {
+    ok: false,
+    message: "Invalid address",
+  }
+}
